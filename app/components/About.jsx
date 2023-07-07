8f61dcb51867d0c300caf246a654c92c3c998f34
aung-myint-ko <aungmyintkoanarmala@gmail.com>
2023-07-07 14:17:40 +0630

    create about page - dev-amk

---
+++ app/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className=" text-2xl mb-2">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+        molestiae temporibus nostrum dolorum quidem nisi architecto, inventore
+        excepturi deserunt ducimus consequuntur sequi distinctio deleniti quos,
+        eum, quia explicabo dolor voluptate.
+      </p>
+    </div>
+  );
+};
+
+export default About;
